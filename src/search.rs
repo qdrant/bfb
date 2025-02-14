c0d1e7851c2ffeac36c7c6bf89918a19e0201883
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2025-02-14 12:42:57 +0100

    Fix sparse query

--- src/search.rs
+++ src/search.rs
@@ -4,7 +4,6 @@ use crate::{random_dense_vector, random_filter, Args};
 use indicatif::ProgressBar;
 use qdrant_client::qdrant::{
     QuantizationSearchParamsBuilder, SearchParamsBuilder, SearchPointsBuilder, SparseIndices,
-    Vector,
 };
 use qdrant_client::Qdrant;
 use std::sync::atomic::AtomicBool;
@@ -50,16 +49,15 @@ impl SearchProcessor {
 
     fn get_sparse_query(&self) -> (Vec<f32>, Option<SparseIndices>, Option<String>) {
         if let Some(sparsity) = self.args.sparse_vectors {
-            let sparse_vector: Vector =
-                random_sparse_vector(self.args.sparse_dim.unwrap_or(self.args.dim), sparsity)
-                    .into();
-            let query_vector = sparse_vector.data;
-            let sparse_indices = sparse_vector.indices;
+            let sparse_vector_tuples =
+                random_sparse_vector(self.args.sparse_dim.unwrap_or(self.args.dim), sparsity);
+            let (indices, values): (Vec<_>, Vec<_>) = sparse_vector_tuples.iter().cloned().unzip();
+            let sparse_indices = SparseIndices { data: indices };
             let name = format!(
                 "{}_sparse",
                 random_vector_name(self.args.sparse_vectors_per_point)
             );
-            (query_vector, sparse_indices, Some(name))
+            (values, Some(sparse_indices), Some(name))
         } else {
             panic!("No sparse vectors configured")
         }
