8b568a7216eefa750ab41ec1e1ba10032f328d40
Ivan Boldyrev <lispnik@gmail.com>
2025-10-29 16:19:40 +0100

    Report RPS too

--- src/main.rs
+++ src/main.rs
@@ -625,6 +625,9 @@ async fn process<P: Processor>(args: &Args, stopped: Arc<AtomicBool>, processor:
     println!("--- Server timings ---");
     print_stats(args, &mut server_timings, "server time", true);
 
+    let mut rps = processor.rps();
+    println!("--- RPS ---");
+    print_stats(args, &mut rps, "rps", false);
     let mut qps = processor.qps();
     println!("--- QPS ---");
     print_stats(args, &mut qps, "qps", false);
@@ -641,7 +644,6 @@ async fn process<P: Processor>(args: &Args, stopped: Arc<AtomicBool>, processor:
         println!("Median precision@10: {p50_time}");
     }
 
-    let rps = processor.rps();
     if args.json.is_some() {
         println!("--- Writing results to json file ---");
         write_to_json(
