30ff7ea914000b4d1607f8e84b54201c9036bc87
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2026-02-10 11:55:36 +0100

    Refactor rand part 1 (#100)
    * Refactor rand part 1
    
    * one step further
    
    * propagate further
    
    * nit
    
    * random vectors

--- src/search.rs
+++ src/search.rs
@@ -12,6 +12,7 @@ use qdrant_client::qdrant::{
     QueryBatchPointsBuilder, QueryPointsBuilder, ScoredPoint, SearchParamsBuilder, SparseIndices,
     VectorInput,
 };
+use rand::Rng;
 
 use std::collections::HashSet;
 use std::sync::atomic::AtomicBool;
@@ -57,16 +58,17 @@ impl SearchProcessor {
         }
     }
 
-    fn get_sparse_queries(&self) -> Vec<(Vec<f32>, Option<SparseIndices>, Option<String>)> {
+    fn get_sparse_queries(&self, rng: &mut impl Rng) -> Vec<(Vec<f32>, Option<SparseIndices>, Option<String>)> {
         if let Some(sparsity) = self.args.sparse_vectors {
             let name = format!(
                 "{}_sparse",
-                random_vector_name(self.args.sparse_vectors_per_point)
+                random_vector_name(rng, self.args.sparse_vectors_per_point)
             );
 
             (0..self.args.search_batch_size)
                 .map(|_| {
                     let sparse_vector_tuples = random_sparse_vector(
+                        rng,
                         self.args.sparse_dim.unwrap_or(self.args.dim),
                         sparsity,
                     );
@@ -81,9 +83,9 @@ impl SearchProcessor {
         }
     }
 
-    fn get_dense_queries(&self) -> Vec<(Vec<f32>, Option<SparseIndices>, Option<String>)> {
+    fn get_dense_queries(&self, rng: &mut impl Rng) -> Vec<(Vec<f32>, Option<SparseIndices>, Option<String>)> {
         let name = if self.args.vectors_per_point > 1 {
-            let name = random_vector_name(self.args.vectors_per_point);
+            let name = random_vector_name(rng, self.args.vectors_per_point);
             Some(name)
         } else {
             None
@@ -92,7 +94,7 @@ impl SearchProcessor {
         (0..self.args.search_batch_size)
             .map(|_| {
                 (
-                    random_dense_vector(self.args.dim, false),
+                    random_dense_vector(rng, self.args.dim, false),
                     None,
                     name.clone(),
                 )
@@ -170,24 +172,25 @@ impl SearchProcessor {
         }
 
         let start = std::time::Instant::now();
-
+        let mut rng = rand::rng();
         let has_sparse = self.args.sparse_vectors.is_some();
         let has_dense = self.args.vectors_per_point > 0;
 
         let use_sparse = match (has_sparse, has_dense) {
-            (true, true) => rand::random::<bool>(),
+            (true, true) => rng.random_bool(0.5),
             (true, false) => true,
             (false, true) => false,
             (false, false) => panic!("No sparse or dense vectors"),
         };
 
         let query_batch = if use_sparse {
-            self.get_sparse_queries()
+            self.get_sparse_queries(&mut rng)
         } else {
-            self.get_dense_queries()
+            self.get_dense_queries(&mut rng)
         };
 
         let query_filter = random_filter(
+            &mut rng,
             &self.args.keywords,
             &self.args.float_payloads,
             &self.args.int_payloads,
